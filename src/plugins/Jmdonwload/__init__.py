6e25b55b1c704017774aa5313fd94c7364ab0113
MiracleNan <2127610576@qq.com>
2025-03-25 13:02:52 +0800

    new1

--- src/plugins/Jmdonwload/__init__.py
+++ src/plugins/Jmdonwload/__init__.py
@@ -130,26 +130,34 @@ clear()
 
 @Jm.handle()
 async def Jm_send(bot: Bot, event: GroupMessageEvent, state: T_State):
-   
+    groupid=event.group_id
+    usrid=event.user_id
     user_msg = str(event.get_message()).strip()
     match = re.match(r'^/jm\s+(\d+)$', user_msg)
-   
+    
     if match:
         # 提取 ID 并将其返回为对象
         manga_id = match.group(1)
-        result = get_comic_info(manga_id)
-       
-        if "error" in result:
+        jminfo = get_comic_info(manga_id)
+        if "error" in jminfo:
             msg = "id不存在或输入错误"
-            await Jm.finish(message=Message(msg))
+            await Jm.finish(message=MessageSegment.at(usrid)+Message(msg))
         else:
             # msg = f"漫画名称: {result['name']}-作者: {result['author']}"
-            name=result['name']
+            name=jminfo['name']
+            
+            groupfiles = await bot.call_api("get_group_root_files", group_id=groupid)
+            group_file_names_set = set(file['file_name'] for file in groupfiles['files'])
+            print(group_file_names_set)
+            if (name+".pdf") in group_file_names_set:
+                await Jm.finish(message=Message(MessageSegment.at(usrid)+'漫画已存在于群文件'))
+            
             option = jmcomic.create_option_by_file('D:/code/jmcomic/option.yml')
             jmcomic.download_album(manga_id,option)
-
+            await bot.send_group_msg(group_id=groupid,message=Message(MessageSegment.at(usrid)+"下载完成，发送中请勿重复请求"))
             file_name = f"{name}.pdf"
             full_path = os.path.join(base_path, file_name)
+
             try:
                 # 调用 upload_group_file API 上传文件
                 await bot.call_api(
@@ -159,17 +167,14 @@ async def Jm_send(bot: Bot, event: GroupMessageEvent, state: T_State):
                     name=file_name
         )
         # 上传成功后发送消息通知
-                await Jm.finish(f"你需要的 {file_name} 已发送")
+                await Jm.finish(Message(MessageSegment.at(usrid)+f" 请求的 {file_name} 已发送"))
             except Exception as e:
         # 上传失败时返回错误信息
                 if str(e)=="FinishedException()":
                     return
-                await Jm.finish(f"文件上传失败：{str(e)}") 
-            # paths=split_image_if_too_long(full_path, max_length=10000)
-            # messages=[]
-            # finalmsg=Message(name)
-            # for path in paths:
-            #     finalmsg+=Message(MessageSegment.image(path))
-            
-            # await Test.finish(message=Message(finalmsg))      
+                if str(e)=='''NetWorkError(message='WebSocket call api upload_group_file timeout')''':
+                    await Jm.finish(Message(MessageSegment.at(usrid)+f" 文件过大"))
+                else:    
+                    await Jm.finish(Message(MessageSegment.at(usrid)+f" 文件上传失败：{str(e)}")) 
+   
 
